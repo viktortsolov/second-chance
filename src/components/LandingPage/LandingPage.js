6d93fd3c6700ff619e5cf52d6150c2142b9dd2b1
Viktor Tsolov <vt@auxcode.com>
2022-07-27 00:33:39 +0300

    Add LandingPage

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,7 +1,19 @@
 import './LandingPage.css';
 
 function LandingPage() {
-    return;
+    return (
+        <main className='Main'>
+            <div className='content'>
+                <h1 className='content__title'>RESELL YOUR BOOKS EASILY</h1>
+                <p className='content__more'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+
+                <div className='content__buttons'>
+                    <button className='button'>SHOW MORE <i class="fa-solid fa-book-open"></i></button>
+                    <button className='button'>OUR LOCATION <i class="fa-solid fa-location-dot"></i></button>
+                </div>
+            </div>
+        </main>
+    );
 }
 
 export default LandingPage;
