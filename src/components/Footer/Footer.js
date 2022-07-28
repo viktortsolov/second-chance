53a0f373cdb11b38d0604c2eb4a35deaaef27a2c
Viktor Tsolov <vt@auxcode.com>
2022-07-28 08:33:46 +0300

    Add Footer and update fontawesome to 6.1.2

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,8 +4,29 @@ function Footer() {
     return (
         <div className='footer'>
             <div className='footer-wrapper'>
-
+                <div className='footer-social'>
+                    <a href=''><i class="fa-brands fa-instagram"></i></a>
+                    <a href=''><i class="fa-brands fa-facebook"></i></a>
+                    <a href=''><i class="fa-brands fa-twitter"></i></a>
+                    <a href=''><i class="fa-solid fa-envelope"></i></a>
+                </div>
             </div>
+
+            <ul className='footer-links'>
+                <li>
+                    <a href=''>Home</a>
+                </li>
+                <li>
+                    <a href=''>Ask us</a>
+                </li>
+                <li>
+                    <a href=''>Contact us</a>
+                </li>
+            </ul>
+
+            <p className='footer-copyright'>
+                &copy; 2022 Second Chance. All Rights Reserved.
+            </p>
         </div>
     )
 }
