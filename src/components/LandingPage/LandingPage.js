71603928768c197d85bb7b613d43776167c0cb38
Viktor Tsolov <vt@auxcode.com>
2022-07-28 08:07:04 +0300

    Add Catalogue

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -2,7 +2,7 @@ import './LandingPage.css';
 
 function LandingPage() {
     return (
-        <main className='Main'>
+        <main className='Main-LandingPage'>
             <div className='content'>
                 <h1 className='content__title'>RESELL YOUR BOOKS EASILY</h1>
                 <p className='content__more'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
