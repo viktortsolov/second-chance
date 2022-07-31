a0d0e3f69ca5b1a7fdd4678cbb50b029cbbc2a14
Viktor Tsolov <vt@auxcode.com>
2022-07-31 22:50:21 +0300

    remove firabase for now

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,12 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDgBBIqOcmD13Mb606HFQ2FAu3FWGLNdmc",
-    authDomain: "second-chance-viktortsolov.firebaseapp.com",
-    databaseURL: "https://second-chance-viktortsolov-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "second-chance-viktortsolov",
-    storageBucket: "second-chance-viktortsolov.appspot.com",
-    messagingSenderId: "441737278016",
-    appId: "1:441737278016:web:449f0971157f5af431a5a5",
-    measurementId: "G-RSMDBG29H2"
+  apiKey: "AIzaSyDgBBIqOcmD13Mb606HFQ2FAu3FWGLNdmc",
+  authDomain: "second-chance-viktortsolov.firebaseapp.com",
+  databaseURL: "https://second-chance-viktortsolov-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "second-chance-viktortsolov",
+  storageBucket: "second-chance-viktortsolov.appspot.com",
+  messagingSenderId: "441737278016",
+  appId: "1:441737278016:web:449f0971157f5af431a5a5",
+  measurementId: "G-RSMDBG29H2"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 export default firebaseConfig;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import * as firebase from 'firebase';
-import firebaseConfig from './firebaseConfig';
-
-firebase.initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
