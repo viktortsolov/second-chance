0edd6fda1f771b5fae2240abdfe92e62e38340ac
Viktor Tsolov <vt@auxcode.com>
2022-08-02 22:55:49 +0300

    Add Profile page with multiple things

--- src/services/Books-Service.js
+++ src/services/Books-Service.js
@@ -4,4 +4,11 @@ export const getAll = () => {
     return fetch(api.books)
         .then(res => res.json())
         .catch(err => console.log(err));
-} 
+}
+
+export const getMine = (userId) => {
+    return fetch(api.books)
+        .then(res => res.json())
+        .then(data => { return data.slice(1).filter(car => car.ownerId === userId) })
+        .catch(err => console.log(err));
+}
