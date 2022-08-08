fb75b5b484c5873f4485f10cf95e80b7020d1406
Viktor Tsolov <vt@auxcode.com>
2022-08-08 20:56:03 +0300

    add firebase

---
+++ src/utils/firebase.js
@@ -0,0 +1,11 @@
+import firebase from "firebase/app";
+import "firebase/storage";
+import "firebase/auth";
+
+import firebaseConfig from "./firebaseConfig";
+
+firebase.initializeApp(firebaseConfig);
+
+const storage = firebase.storage();
+
+export { storage, firebase as default }; 
