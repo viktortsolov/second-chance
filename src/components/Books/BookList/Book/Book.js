644b40e84ddd90211487e6a5fb68fb71e167d343
Viktor Tsolov <vt@auxcode.com>
2022-08-10 12:43:49 +0300

    Update Book element

--- src/components/Books/BookList/Book/Book.js
+++ src/components/Books/BookList/Book/Book.js
@@ -4,49 +4,26 @@ import './Book.css';
 function Book({ book }) {
     return (
         <li className="Book">
-             <Link to={`/book/${book.id}`}>
-                <article className="Book-info">
-                    <div className="info__picBox">
-                        <img src={book.picture} alt="No Photo available" className="pic" />
-                    </div>
-                    <div className="info__content">
-                        <label htmlFor="title">
-                            <strong>Title: </strong>
-                            <p id="title" className="content__title">{book.title}</p>
-                        </label>
-
-                        <label htmlFor="author">
-                            <strong>Author: </strong>
-                            <p id="author" className="content__author">{book.author}</p>
-                        </label>
-
-                        <label htmlFor="year">
-                            <strong>Year: </strong>
-                            <p id="year" className="content__year">{book.year}</p>
-                        </label>
-
-                        <label htmlFor="genre">
-                            <strong>Genre: </strong>
-                            <p id="genre" className="content__genre">{book.genre}</p>
-                        </label>
-
-                        <label htmlFor="pages">
-                            <strong>Pages: </strong>
-                            <p id="pages" className="content__pages">{book.pages}</p>
-                        </label>
-
-                        <label htmlFor="make">
-                            <strong>Make: </strong>
-                            <p id="make" className="content__make">{book.make}</p>
-                        </label>
-
-                        <label htmlFor="quality">
-                            <strong>Quality: </strong>
-                            <p id="quality" className="content__quality">{book.quality}</p>
-                        </label>
-                    </div>
-                </article>
-            </Link>
+            <div className="info__picBox">
+                <img src={book.picture} alt="No Photo available" className="pic" />
+            </div>
+            <div className="info__content">
+                <div className="info__text">
+                    <p id="title" className="content__title">{book.title}</p>
+                    <p id="info">
+                        <p id="author" className="content__author">{book.author}</p>
+                        <p id="year" className="content__year">{book.year}</p>
+                        <p id="genre" className="content__genre">{book.genre}</p>
+                        <p id="pages" className="content__pages">{book.pages} pages</p>
+                        <p id="make" className="content__make">{book.make}</p>
+                        <p id="quality" className="content__quality">{book.quality} quality</p>
+                        <p id="price" className="content__price"><strong>Price: </strong>{book.price} $</p>
+                        <Link className="submitBtn" to={`/book/${book.id}`}>
+                            Description
+                        </Link>
+                    </p>
+                </div>
+            </div>
         </li>
     );
 }
